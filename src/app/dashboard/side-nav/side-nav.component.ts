d99cb1bb640df31f38e076846bf9ca1fbe5d0c15
olafsitkowski <olaf963@op.pl>
2023-04-15 12:48:12 +0000

    upgraded navbar

--- src/app/dashboard/side-nav/side-nav.component.ts
+++ src/app/dashboard/side-nav/side-nav.component.ts
@@ -1,13 +1,29 @@
-import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit {
   constructor(private router: Router) {}
+
+  public currentRoute: string = '';
+
+  public ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+        localStorage.setItem('currentRoute', this.currentRoute);
+      }
+    });
+
+    const storedRoute = localStorage.getItem('currentRoute');
+    if (storedRoute) {
+      this.currentRoute = storedRoute;
+    }
+  }
 
   public changeRoute(route: string): void {
     this.router.navigate([`/dashboard/${route}`]);
